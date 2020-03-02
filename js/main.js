4ca2a337b47f8982a4dfa05d87db083f16f1c5db
Ionut Birlad <birladionut@yahoo.it>
2020-03-02 15:12:01 +0100

    aggiunto il previuos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
 
     // Invoco le funzioni per cambiare slide
+
+    /* ==> Opzione 1
     $('.next').click(nextSlide);        // Al click richiamo la funzione per aggiornare l'immagine visualizzando la successiva
+    $('.prev').click(prevSlide);        // Al click richiamo la funzione per aggiornare l'immagine visualizzando la precedente
+    */
+
+    // OPzione 2
+    $(".next").click(function () {
+      nextSlide();
+    });
+    $(".prev").click(function () {
+      prevSlide();
+    });
 
 
     // Definisco le funzioni nextImage e prevImage
@@ -28,6 +40,33 @@ $(document).ready(function() {
 
             pallinoAttivo.removeClass('active');
             prossimaPallino.addClass('active');
+        }
+    }
+
+    // FUNZIONE PrevSlide
+    function prevSlide() {
+        if ( $('.images img.active').hasClass('first') ) {
+
+            $('.images img.active').removeClass('active');
+            $('.images img.last').addClass('active');
+
+            // Pallini
+            $('.slider-nav i.active').removeClass('active');
+            $('.slider-nav i.last').addClass('active');
+
+        } else {
+            var imgAttiva = $('.images img.active'); // Rimuovo l'active
+            var prevImg = $('.images img.active').prev();
+
+            imgAttiva.removeClass('active');
+            prevImg.addClass('active');
+
+            // Pallini
+            var pallinoAttivo = $('.slider-nav i.active'); // Rimuovo l'active
+            var prevPallino = $('.slider-nav i.active').prev();
+
+            pallinoAttivo.removeClass('active');
+            prevPallino.addClass('active');
         }
     }
 
