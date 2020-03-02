cebef69ba2e99d875d43764a82320df6077a05d2
Ionut Birlad <birladionut@yahoo.it>
2020-03-02 15:16:29 +0100

    aggiunta parte 2

---
+++ parte_2/js/main.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+    // Invoco le funzioni per cambiare slide
+
+    /* ==> Opzione 1
+    $('.next').click(nextSlide);        // Al click richiamo la funzione per aggiornare l'immagine visualizzando la successiva
+    $('.prev').click(prevSlide);        // Al click richiamo la funzione per aggiornare l'immagine visualizzando la precedente
+    */
+
+    // OPzione 2
+    $(".next").click(function () {
+      nextSlide();
+    });
+    $(".prev").click(function () {
+      prevSlide();
+    });
+
+
+    // Definisco le funzioni nextImage e prevImage
+    function nextSlide() {
+        if ( $('.images img.active').hasClass('last') ) {
+
+            $('.images img.active').removeClass('active');
+            $('.images img.first').addClass('active');
+
+            // Pallini
+            $('.slider-nav i.active').removeClass('active');
+            $('.slider-nav i.first').addClass('active');
+
+        } else {
+            var imgAttiva = $('.images img.active'); // Rimuovo l'active
+            var prossimaImg = $('.images img.active').next();
+
+            imgAttiva.removeClass('active');
+            prossimaImg.addClass('active');
+
+            // Pallini
+            var pallinoAttivo = $('.slider-nav i.active'); // Rimuovo l'active
+            var prossimaPallino = $('.slider-nav i.active').next();
+
+            pallinoAttivo.removeClass('active');
+            prossimaPallino.addClass('active');
+        }
+    }
+
+    // FUNZIONE PrevSlide
+    function prevSlide() {
+        if ( $('.images img.active').hasClass('first') ) {
+
+            $('.images img.active').removeClass('active');
+            $('.images img.last').addClass('active');
+
+            // Pallini
+            $('.slider-nav i.active').removeClass('active');
+            $('.slider-nav i.last').addClass('active');
+
+        } else {
+            var imgAttiva = $('.images img.active'); // Rimuovo l'active
+            var prevImg = $('.images img.active').prev();
+
+            imgAttiva.removeClass('active');
+            prevImg.addClass('active');
+
+            // Pallini
+            var pallinoAttivo = $('.slider-nav i.active'); // Rimuovo l'active
+            var prevPallino = $('.slider-nav i.active').prev();
+
+            pallinoAttivo.removeClass('active');
+            prevPallino.addClass('active');
+        }
+    }
+
+
+
+
+});
